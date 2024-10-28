36ed059165714936317f35534f0cf5f2652459c5
Sophia Dai <sdai33@smith.edu>
2024-10-28 13:52:07 -0400

    naive gpu

---
+++ naive.cu
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <chrono>  
+#include <iostream>
+
+#define SIZE 128
+__global__ void scanKernel(int* d_out, int* d_in) {
+    __shared__ int temp[SIZE]; 
+
+    int tid = threadIdx.x;
+
+
+    temp[tid] = d_in[tid];
+    __syncthreads();
+
+
+    int value = 0;
+    for (int i = 0; i <= tid; i++) {
+        value += temp[i];
+    }
+
+    d_out[tid] = value;
+}
+
+int main() {
+    int* h_input = (int*)malloc(sizeof(int) * SIZE);
+    int* h_output = (int*)malloc(sizeof(int) * SIZE);
+    int* d_input;
+    int* d_output;
+
+    
+    for (int i = 0; i < SIZE; i++) {
+        h_input[i] = 1;  
+    }
+    auto start = std::chrono::high_resolution_clock::now();
+
+   
+    cudaMalloc((void**)&d_input, sizeof(int) * SIZE);
+    cudaMalloc((void**)&d_output, sizeof(int) * SIZE);
+
+ 
+    cudaMemcpy(d_input, h_input, sizeof(int) * SIZE, cudaMemcpyHostToDevice);
+
+    
+    scanKernel<<<1, SIZE>>>(d_output, d_input);
+
+
+    cudaMemcpy(h_output, d_output, sizeof(int) * SIZE, cudaMemcpyDeviceToHost);
+
+    auto end = std::chrono::high_resolution_clock::now();
+    
+    std::chrono::duration<double> elapsed = end - start;
+    printf("Scan result:\n");
+    for (int i = 0; i < SIZE; i++) {
+        printf("%d ", h_output[i]);
+    }
+    printf("\n");
+
+    std::cout << "Time taken for naive GPU prefix sum: " << elapsed.count() << " seconds" << std::endl;
+    cudaFree(d_input);
+    cudaFree(d_output);
+
+
+    free(h_input);
+    free(h_output);
+
+    return 0;
+}

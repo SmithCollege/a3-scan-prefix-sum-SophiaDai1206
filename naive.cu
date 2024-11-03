502608d115ae32566a58bc3f01605ffa979ff5d4
Sophia Dai <sdai33@smith.edu>
2024-11-03 13:24:26 -0500

    Update naive.cu

--- naive.cu
+++ naive.cu
@@ -3,12 +3,11 @@
 #include <chrono>  
 #include <iostream>
 
-#define SIZE 128
+#define SIZE 1024
 __global__ void scanKernel(int* d_out, int* d_in) {
     __shared__ int temp[SIZE]; 
 
-    int tid = threadIdx.x;
-
+    int tid = threadIdx.x + blockIdx.x * blockDim.x;
 
     temp[tid] = d_in[tid];
     __syncthreads();
@@ -32,7 +31,7 @@ int main() {
     for (int i = 0; i < SIZE; i++) {
         h_input[i] = 1;  
     }
-    auto start = std::chrono::high_resolution_clock::now();
+    
 
    
     cudaMalloc((void**)&d_input, sizeof(int) * SIZE);
@@ -41,13 +40,10 @@ int main() {
  
     cudaMemcpy(d_input, h_input, sizeof(int) * SIZE, cudaMemcpyHostToDevice);
 
-    
+    auto start = std::chrono::high_resolution_clock::now();
     scanKernel<<<1, SIZE>>>(d_output, d_input);
-
-
-    cudaMemcpy(h_output, d_output, sizeof(int) * SIZE, cudaMemcpyDeviceToHost);
-
     auto end = std::chrono::high_resolution_clock::now();
+    cudaMemcpy(h_output, d_output, sizeof(int) * SIZE, cudaMemcpyDeviceToHost);
     
     std::chrono::duration<double> elapsed = end - start;
     printf("Scan result:\n");
